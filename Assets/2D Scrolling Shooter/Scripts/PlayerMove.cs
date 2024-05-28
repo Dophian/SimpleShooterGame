c7faf2cef85441318e40654c74dd782244702d76
Dophian <169108628+Dophian@users.noreply.github.com>
2024-05-28 09:52:30 +0900

    플레이어의 X/Y 위치를 조정할 때 사용할 클래스 추가

--- Assets/2D Scrolling Shooter/Scripts/PlayerMove.cs
+++ Assets/2D Scrolling Shooter/Scripts/PlayerMove.cs
@@ -3,8 +3,37 @@ using UnityEngine;
 // 마우스 드래그(모바일-터치)를 사용해서 플레이어를 이동시키는 스크립트.
 public class PlayerMove : MonoBehaviour
 {
+    // 최소/최대 범위를 지정할 때 사용할 클래스 정의.
+    
+    // 우리가 선언한 클래스는 유니티가 모름.
+    // 이 클래스를 인스펙터에 노출하려면, 명시적으로 아래 태그를 추가해야함.
+    [System.Serializable]
+    class ClampValue
+    {
+        // 범위를 지정할 때 사용할 최소값.
+        [SerializeField] private float min;
+
+        // 범위를 지정할 때 사용할 최대값.
+        [SerializeField] private float max;
+
+        public float Min { get { return min; } }
+        public float Max { get { return max; } }
+
+        // 전달 받은 값을 min과 max 사이의 값으로 고정해주는 함수.
+        public float Clamp(float target)
+        {
+            return Mathf.Clamp(target, min, max);
+        }
+    }
+
     // 플레이어가 이동할 때 적용할 딜레이 속도 값.
     [SerializeField] private float lagSpeed = 5f;
+
+    // x 위치에 사용할 범위 변수.
+    [SerializeField] private ClampValue clampX;
+
+    // y 위치에 사용할 범위 변수.
+    [SerializeField] private ClampValue clampY;
 
     // 카메라를 저장할 참조 변수.
     private Camera mainCamera;
@@ -50,7 +79,12 @@ public class PlayerMove : MonoBehaviour
             Vector3 targetPosition = clickPosition + offset;
 
             // x 축의 위치를 화면을 벗어나지 않도록 설정.
-            targetPosition.x = Mathf.Clamp(targetPosition.x, -1.05f, 1.05f);
+            //targetPosition.x = Mathf.Clamp(targetPosition.x, clampX.Min, clampX.Max);
+            targetPosition.x = clampX.Clamp(targetPosition.x);
+
+            // y축의 위치를 조정.
+            //targetPosition.y = Mathf.Clamp(targetPosition.y, clampX.Min, clampX.Max);
+            targetPosition.y = clampY.Clamp(targetPosition.y);
 
             // 3차원으로 변환을 거친 위치를 플레이어의 위치로 설정.
             //refTransform.position = clickPosition + offset;
