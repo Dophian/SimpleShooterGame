69ee96077056469d35ae4e89b6b7a3d36ee16377
Dophian <169108628+Dophian@users.noreply.github.com>
2024-05-31 16:43:30 +0900

    적 발사 로직 개선 및 적 스포닝 시스템 추가
    - 적 캐릭터가 특정  y값 이하로 내려가면 발사 안하도록 함
    - 적 그룹을 스포닝(생성)하는 시스템을 추가

--- Assets/2D Scrolling Shooter/Scripts/Enemy/EnemyShoot.cs
+++ Assets/2D Scrolling Shooter/Scripts/Enemy/EnemyShoot.cs
@@ -10,6 +10,9 @@ public class EnemyShoot : MonoBehaviour
     // 탄약의 속력 (빠르기, 단위:초).
     [SerializeField] private float bulletSpeed = 3f;
 
+    // 탄약의 발사를 제한하는 Y 높이 값.
+    [SerializeField] private float shootStopYPosition = -2f;
+
     // 탄약 프리팹.
     [SerializeField] private GameObject bulletPrefab;
 
@@ -53,8 +56,19 @@ public class EnemyShoot : MonoBehaviour
             return;
         }
 
-        // 플레이어의 위치를 향하는 방향 구하기.
+        if (transform.position.y < shootStopYPosition )
+        {
+            return;
+        }
+
+        // 플레이어의 위치를 향하는 방향 구하기
         Vector3 direction = refPlayer.position - transform.position;
+
+        //// 플레이어가 적 캐릭터 앞에 있는지 뒤에 있는지 확인 후 앞에 있으면 발사.
+        //if (Vector3.Dot(-transform.up, direction.normalized) < 0f)
+        //{
+        //    return;
+        //}
 
         // 프리팹을 이용해 탄약을 생성하고,
         var newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
